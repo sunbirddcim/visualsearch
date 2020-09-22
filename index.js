373b4f271bc5d96e06ec542b21b403e528d2d5e3
Joe Tsai <joe.tsai@sunbirddcim.com>
2020-09-22 15:55:05 +0800

    test

--- index.js
+++
@@ -1,8 +0,0 @@
-// import "jquery-ui/ui/widget"
-// import "jquery-ui/ui/position"
-// import "jquery-ui/ui/widgets/menu"
-// import "jquery-ui/ui/widgets/autocomplete";
-
-// import 'visualsearch/build/dependencies';
-// import 'visualsearch/build/visualsearch';
-// export {VS};
